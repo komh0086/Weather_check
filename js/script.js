eef8e39b990ad9080293f9428afae4a7a8eb8157
고민혁 <rhalsgur0086@gmail.com>
2023-08-04 00:28:02 +0900

    [ADD] background img [MOD] CSS, JS, HTML

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-function getWeather(itemCode) {
+function getWeather(msrstnName) {
     const apiKey = 'Ak9aORXhv2N8IxiN%2FVh%2BjKM7sDM3NVwmAwv9nTf9TJShIhimCh1dY7P%2FZqGHOdbnQSLF%2FsDUcgp3D6u0ISF%2ByQ%3D%3D';
     const city = document.getElementById('cityInput').value;
+    var date = getToday;
 
-    fetch(`http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst?serviceKey=${apiKey}&returnType=json&numOfRows=30&pageNo=1&itemCode=${itemCode}&dataGubun=HOUR&searchCondition=WEEK`)
+    fetch(`http://apis.data.go.kr/B552584/ArpltnStatsSvc/getMsrstnAcctoRDyrg?inqBginDt=${date}&inqEndDt=${date}&msrstnName=${msrstnName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            const weatherDataDiv = document.getElementById('weatherData');
-
-            if (data.cod === 200) {
-                const weatherDescription = data.weather[0].description;
-                const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
-
-                weatherDataDiv.innerHTML = `Weather: ${weatherDescription}<br>Temperature: ${temperature}°C`;
-            } else {
-                weatherDataDiv.innerHTML = 'City not found. Please try again.';
-            }
+            console.log("api 요청 성공");
+            //직렬화
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
         });
+
+    function getToday(){
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = ("0" + (1 + date.getMonth())).slice(-2);
+        var day = ("0" + date.getDate()).slice(-2);
+
+        return year + month + day;
+    }
 }
