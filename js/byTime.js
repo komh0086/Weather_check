4c66cd422bdcaf6953a654118c546edebc947b1f
komh0086 <rhalsgur0086@gmail.com>
2023-08-16 11:16:58 +0900

    대기질 예보 화면 구성 완료

--- js/byTime.js
+++ js/byTime.js
@@ -1,41 +1,38 @@
 let total_data = {
 };
-
-let list = ['PM10', 'PM25', 'O3', 'NO2', 'CO', 'SO2'];
+let list = ["PM10", "PM25", "O3"];
 let btn_list = document.getElementsByClassName('item_btn');
 let image = document.getElementById('image');
 let img_index = 0;
 
-let img_list = [
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM10.1hsp.2023081300.png',
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM10.1hsp.2023081306.png',
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM10.1hsp.2023081312.png',
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM2P5.1hsp.2023081312.png',
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM2P5.1hsp.2023081306.png',
-    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/13/AQFv1_21h.20230812.KNU_09_01.PM2P5.1hsp.2023081300.png'
-];
+let img_url = 
+    'https://www.airkorea.or.kr/file/proxyImage?fileName=2023/08/15/AQFv1_09h.20230815.KNU_09_01.PM10.2days.ani.gif'
+;
 
 init();
 
-function changeImage(){
-
-    image.src = img_list[img_index%img_list.length];
-    img_index++;
-}
 function init(){
-    // for(var i=0;i<list.length;i++){
-    //     getWeather(list[i]);
-    // }
-    getWeather("PM10");
-
     for(var i=0;i<list.length;i++){
-        btn_list[i].addEventListener('click', (e) =>{
-            showData(list[i]);
-            e.target.style.color = 'blue';
-            e.target.style.borderColor = 'blue';
-        });
+        getWeather(list[i]);
     }
-    setInterval(changeImage, 1000);
+
+    btn_list[0].addEventListener('click', (e) =>{
+        showData("PM10");
+        e.target.style.color = 'blue';
+        e.target.style.borderColor = 'blue';
+    });
+
+    btn_list[1].addEventListener('click', (e) =>{
+        showData("PM25");
+        e.target.style.color = 'blue';
+        e.target.style.borderColor = 'blue';
+    });
+
+    btn_list[2].addEventListener('click', (e) =>{
+        showData("O3");
+        e.target.style.color = 'blue';
+        e.target.style.borderColor = 'blue';
+    });
 }
 
 function showData(itemCode){
@@ -44,27 +41,60 @@ function showData(itemCode){
         btn_list[i].style.color = 'grey';
     }
 
-    // document.getElementById("inform")
-    //     .innerHTML = "예보정보" 
-    //         + total_data[itemCode]["informOverall"]
-    //         + total_data[itemCode]["informCause"];
-    // document.getElementById("inform").innerHTML = itemCode;
+    var regions = [
+        "busan", "chungbuk", "chungnam", "daegu", "daejeon",
+        "gyeonggibukbu", "gwangju", "gyeongbuk", "youngdong", "gyeongnam",
+        "incheon", "jeju", "jeonbuk", "jeonnam", "sejong", "seoul", "ulsan"
+    ];
+    var regions_korean = [
+        "부산", "충북", "충남", "대구", "대전", "경기북부", "광주", "경북", "영동", "경남",
+        "인천", "제주", "전북", "전남", "세종", "서울", "울산"
+    ];
+
+    var informGrade_list = {};
+    informGrade_list = total_data['PM10'].informGrade.replaceAll(" : ", ",").split(",");
     
+    for(var i=0;i<informGrade_list.length;i+=2){
+        for(var j=0;j<regions_korean.length;j++){
+            if(regions_korean[j] == informGrade_list[i]){
+                var div = document.getElementById(regions[j]);
+                div.innerHTML = regions_korean[j] + " : " + informGrade_list[i+1];
+                if(informGrade_list[i+1]=='좋음'){
+                    div.style.borderColor = 'blue';
+                }else if(informGrade_list[i+1] == '보통'){
+                    div.style.borderColor = 'orange';
+                }else{
+                    div.style.borderColor = 'red';
+                }
+                break;
+            }
+        }
+    }
+    var image = document.getElementById("image");
+    if(itemCode == "PM10")
+        image.src = total_data['PM10'].imageUrl7;
+    else if(itemCode == "PM25")
+        image.src = total_data['PM25'].imageUrl8;
+    else
+        image.src = total_data['O3'].imageUrl9;
+
+    document.getElementById("Overall").innerHTML = total_data[itemCode].informOverall;
+    document.getElementById("Cause").innerHTML = total_data[itemCode].informCause;
+
 }
 
 function getWeather(itemCode) {
     const apiKey = 'Ak9aORXhv2N8IxiN%2FVh%2BjKM7sDM3NVwmAwv9nTf9TJShIhimCh1dY7P%2FZqGHOdbnQSLF%2FsDUcgp3D6u0ISF%2ByQ%3D%3D';
     var date = getToday();
-    var url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?serviceKey=${apiKey}&returnType=json&numOfRows=1&pageNo=1&searchDate=${date}&informCode=${itemCode}`;
-    console.log(url);
+    var url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?serviceKey=${apiKey}&returnType=json&numOfRows=1&pageNo=1&searchDate=${date}&informCode=${itemCode}&ver=1.1`;
+    
     fetch(url)
         .then(response => response.json())
         .then(data => {
             let res = data.response.body.items[0];
             total_data[itemCode] = res;
-            
             })
-        .catch(error => {
+        .catch(error  => {
             console.error(error);
         });
 }
